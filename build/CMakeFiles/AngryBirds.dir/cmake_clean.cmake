e9a5762db9febe4fd09af63f703ca3a352ad1cf6
Casper Tillander <casper.tillander@aalto.fi>
2023-11-18 19:25:36 +0200

    Working integration of both box2d and SFML, as well as a testfile to show that they are working

---
+++ build/CMakeFiles/AngryBirds.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AngryBirds"
+  "AngryBirds.pdb"
+  "CMakeFiles/AngryBirds.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AngryBirds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
